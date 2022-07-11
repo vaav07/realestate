ce5b22a3a7a8979ddf36c12f3648e8848fabe404
Vaibhav <v071997@gmail.com>
2022-07-11 21:08:07 +0530

    first commit

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,7 +5,7 @@ export const baseUrl = "https://bayut.p.rapidapi.com";
 export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
-      "X-RapidAPI-Key": '73b1cad459mshd60ca37688aca70p16f3f7jsn4c3fb60b801a',
+      "X-RapidAPI-Key": process.env.KEY,
       "X-RapidAPI-Host": "bayut.p.rapidapi.com",
     },
   });
